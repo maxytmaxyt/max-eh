d7351a8adb3876ceeb4dc49299269cbfac4dfbec
Max Msuti <163127355+maxytmaxyt@users.noreply.github.com>
2025-11-19 08:34:32 +0100

    Update Footer.js

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="mt-20 p-6 text-center border-t border-white/10 shadow-[0_0_10px_#00eaff]">
       <p className="text-white/70">
-        &copy; {new Date().getFullYear()} Vikiluxia Studios. Alle Rechte vorbehalten.
+        &copy; {new Date().getFullYear()} Cityblitz. Alle Rechte vorbehalten.
       </p>
     </footer>
   );
